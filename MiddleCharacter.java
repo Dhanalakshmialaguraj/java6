ce4fe2851a0c892fa66fd9c3ab2d785b9730568b
Dhanalakshmialaguraj <rjdhana41107@gmail.com>
2025-03-07 12:22:35 +0530

    Update MiddleCharacter.java

--- MiddleCharacter.java
+++ MiddleCharacter.java
@@ -1,1 +1,20 @@
+class MiddleCharacter {
+    // Method to get the middle character(s) of a string
+    public static String getMiddleCharacter(String str) {
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("String cannot be null or empty");
+        }
+
+        int length = str.length();
+        int middle = length / 2;
+
+        // If the length is odd, return one character, else return two characters
+        return (length % 2 == 0) ? str.substring(middle - 1, middle + 1) : str.substring(middle, middle + 1);
+    }
+
+    public static void main(String[] args) {
+        String input = "Jenkins"; // Change this to test different inputs
+        System.out.println("Middle character(s) of \"" + input + "\": " + getMiddleCharacter(input));
+    }
+}
 
